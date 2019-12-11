339688b42808dccd50bedb785efefb646305326f
이도현 <idohyeon@idohyeon-ui-MacBookPro.local>
2019-12-11 14:42:42 +0900

    added markup to views.py -not recommended

--- blog/views.py
+++ blog/views.py
@@ -4,4 +4,12 @@ from django.shortcuts import render
 
 def post_list(request):
     #HttpResponse는 클래스 이다.
-    return HttpResponse('<html><body><h1>Post List</h1></body></html>')
+    return HttpResponse('<html>'
+                        '<body>'
+                        '<h1>Post List</h1>'
+                        '<div><p>published:12.11.2019</p>'
+                        '<h2><a href=#>My first post</a></h2>'
+                        '<div><p>published:12.12.2019</p>'
+                        '<h2><a href=#>My second post</a></h2>'
+                        '</body>'
+                        '</html>')
