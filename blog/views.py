bff48a81153f1893e18b2e6f47ec0e8f94bb1f5b
이도현 <idohyeon@idohyeon-ui-MacBookPro.local>
2019-12-11 14:35:39 +0900

    added functions to views.py

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,7 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+
+def post_list(request):
+    #HttpResponse는 클래스 이다.
+    return HttpResponse('<html><body><h1>Post List</h1></body></html>')
